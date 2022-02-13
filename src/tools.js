925e3c430bbd335bfd71c85738361cd9e0a953f5
Tim Franklin <timsfranklin1@gmail.com>
2022-02-13 15:27:46 -0500

    added an option to format for vmeets

--- src/tools.js
+++ src/tools.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const path = require("path");
-const cheerio = require("cheerio");
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
 
 function processArgs(args) {
   if (!args || args.length < 3) {
-    console.error("Invalid arguments: Must provide config file");
+    console.error('Invalid arguments: Must provide config file');
     return;
   }
   const configPath = process.argv[2];
@@ -12,18 +12,18 @@ function processArgs(args) {
 }
 
 function mergeFiles({ files }) {
-  console.log("files: ", files);
-  let finalText = "";
+  console.log('files: ', files);
+  let finalText = '';
 
   for (let f of files) {
     finalText +=
-      "\n\n<!-- ***************************************  START " +
+      '\n\n<!-- ***************************************  START ' +
       f +
-      "   ****************************************** -->\n\n";
+      '   ****************************************** -->\n\n';
     try {
-      finalText += fs.readFileSync(f, "utf8");
+      finalText += fs.readFileSync(f, 'utf8');
     } catch (error) {
-      console.error("\x1b[31m", "ERROR Reading files:\n", error.message);
+      console.error('\x1b[31m', 'ERROR Reading files:\n', error.message);
       return;
     }
   }
@@ -33,28 +33,28 @@ function mergeFiles({ files }) {
 function urlRaplacer(urlMappings, $) {
   for (let i of urlMappings) {
     const { id, src } = i;
-    const $el = $("#" + id.toString());
-    console.log("before type");
-    const elType = $($el).prop("nodeName");
-    console.log("el type: ", elType, typeof elType);
+    const $el = $('#' + id.toString());
+    console.log('before type');
+    const elType = $($el).prop('nodeName');
+    console.log('el type: ', elType, typeof elType);
 
     let attr;
 
     switch (elType) {
-      case "SCRIPT":
-        attr = "src";
-        console.log("is script");
+      case 'SCRIPT':
+        attr = 'src';
+        console.log('is script');
         break;
 
-      case "STYLE":
-        attr = "href";
+      case 'STYLE':
+        attr = 'href';
         break;
 
       default:
         break;
     }
     if (attr) {
-      console.log("attr: ", attr);
+      console.log('attr: ', attr);
       $el.attr(attr, src);
     }
   }
@@ -63,28 +63,28 @@ function urlRaplacer(urlMappings, $) {
 function jsFileReplacer(js_sources, $) {
   for (let js of js_sources) {
     const { id, src } = js;
-    console.log("js", id);
-    let jsFile = fs.readFileSync(src, "utf8");
-    let startRegion = "#region----- start " + src + " -----";
-    let endRegion = "#endregion------- end " + src + " ------";
+    console.log('js', id);
+    let jsFile = fs.readFileSync(src, 'utf8');
+    let startRegion = '#region----- start ' + src + ' -----';
+    let endRegion = '#endregion------- end ' + src + ' ------';
     // jsFile = "\n\n//#region----- start " + src + " -----\n\n" + jsFile;
     // jsFile += "\n\n//endregion------- end " + src + " ------\n\n";
 
-    const $el = $("#" + id);
-    const elType = $el.prop("nodeName");
+    const $el = $('#' + id);
+    const elType = $el.prop('nodeName');
 
     switch (elType) {
-      case "SCRIPT":
-        startRegion = "\n\n//" + startRegion + "\n\n";
-        endRegion = "\n\n//" + endRegion + "\n\n";
+      case 'SCRIPT':
+        startRegion = '\n\n//' + startRegion + '\n\n';
+        endRegion = '\n\n//' + endRegion + '\n\n';
         jsFile = startRegion + jsFile + endRegion;
-        $el.removeAttr("src").html(jsFile);
+        $el.removeAttr('src').html(jsFile);
 
         break;
 
-      case "LINK":
-        startRegion = "/*" + startRegion + "*/\n\n";
-        endRegion = "\n\n/*" + endRegion + "*/\n\n";
+      case 'LINK':
+        startRegion = '/*' + startRegion + '*/\n\n';
+        endRegion = '\n\n/*' + endRegion + '*/\n\n';
         jsFile = startRegion + jsFile + endRegion;
         $el.after($(`<style>`).html(jsFile));
         $el.remove();
@@ -97,8 +97,13 @@ function jsFileReplacer(js_sources, $) {
   }
 }
 
+function formatForVmeet(renderedHtml, $) {
+  const vmeetFormatted = `</style>${renderedHtml}<style>`;
+  return vmeetFormatted;
+}
+
 function formatHtml(config) {
-  const parsedConfig = JSON.parse(fs.readFileSync(config, "utf8"));
+  const parsedConfig = JSON.parse(fs.readFileSync(config, 'utf8'));
   const { html_sources, url_mapping, output, js_sources } = parsedConfig;
   console.log(html_sources);
   const htmlString = mergeFiles({ files: html_sources });
@@ -110,7 +115,11 @@ function formatHtml(config) {
   urlRaplacer(url_mapping, $);
   jsFileReplacer(js_sources, $);
   // * render the html
-  const renderedHtml = $("body").html();
+  let renderedHtml = $('body').html();
+  if (renderedHtml.match(/\*\*VMEET\*\*/g)) {
+    renderedHtml = renderedHtml.replace(/\/\/.*\*\*VMEET\*\*/g, '');
+    renderedHtml = formatForVmeet(renderedHtml);
+  }
 
   // * write rendered html to file specified in config
   writeFile(output, renderedHtml);
